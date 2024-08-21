c4ab57f6d75370989bae4cfcd4cf4b0619965b08
Eduardo Barredo <ebarredo@gmail.com>
2024-08-21 15:53:00 +0200

    Updated macros for setting GPIO level as GPIO16 uses a different register

--- src/sender/common.h
+++ src/sender/common.h
@@ -2,9 +2,12 @@
 
 #include "driver/gpio.h"
 
-// GPIO Mappings - By default, GPIO1 and GPIO3 are reserved for UART
+// GPIO Mappings
+//
+// - GPIO1 and GPIO3 are externally connected to the UART interface of CH340C
+// - GPIO16 is internally connected to the RTC module
 
-#define XRST_PORT       GPIO_NUM_16 // D0
+#define XRST_PORT       GPIO_NUM_16 // D0 (RTC)
 #define LED_PORT        GPIO_NUM_2  // D4
 #define CLK_PORT        GPIO_NUM_14 // D5
 #define XLT_PORT        GPIO_NUM_12 // D6
@@ -16,10 +19,18 @@
 #define SPI_REG_BASE    0x60000200
 
 // Sets GPIO output LO (GND)
-#define SET_LO(n)       WRITE_PERI_REG(SPI_REG_BASE + 0x108 /* L */, 1 << n)
+#define SET_LO(n)       if (n == GPIO_NUM_16) { \
+                          CLEAR_PERI_REG_MASK(RTC_GPIO_OUT, 0x1); \
+                        } else { \
+                          WRITE_PERI_REG(SPI_REG_BASE + 0x108 /* L */, 1 << n); \
+                        }
 
 // Sets GPIO output HI (VDD)
-#define SET_HI(n)       WRITE_PERI_REG(SPI_REG_BASE + 0x104 /* H */, 1 << n)
+#define SET_HI(n)       if (n == GPIO_NUM_16) { \
+                          SET_PERI_REG_MASK(RTC_GPIO_OUT, 0x1); \
+                        } else { \
+                          WRITE_PERI_REG(SPI_REG_BASE + 0x104 /* H */, 1 << n); \
+                        }
 
 // Macro for converting a value given in uS to ticks for the FRC Timer
 // Clock divider must be set to TIMER_CLKDIV_16
